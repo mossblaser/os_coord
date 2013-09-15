cf30fefa94fa8fc0c34e025a549ea2d1b3ff11b0
Jonathan Heathcote <mail@jhnet.co.uk>
2013-09-15 19:09:51 +0100

    Added avr_f64 option for math (untested).

--- os_coord_math.h
+++ os_coord_math.h
@@ -12,44 +12,88 @@
 #ifndef OS_COORD_MATH_H
 #define OS_COORD_MATH_H
 
-#include <math.h>
+// Uncomment to use the avr_f64 library for arithmetic
+//#define USE_AVR_F64
 
-// Use double precision floats
-#define OS_NUM_T double
-
-// Do whatever is needed to represent a literal. Assumes the argument is a
-// C double.
-#define OS_NUM_LIT(n) (n)
-
-// Convert to an integer (flooring the value)
-#define OS_NUM2INT(n) ((int)(n))
-
-// Convert an integer to a number
-#define OS_INT2NUM(i) ((double)(i))
-
-// Compare: 0 if equal, -ve if a < b, +ve if a > b
-#define OS_CMP(a,b) ((a)-(b))
-
-// Basic arithmetic
-#define OS_ADD(a,b) ((a)+(b))
-#define OS_SUB(a,b) ((a)-(b))
-#define OS_MUL(a,b) ((a)*(b))
-#define OS_DIV(a,b) ((a)/(b))
-
-// Advanced arithmetic
-#define OS_POW(n,p) (pow((n),(p)))
-#define OS_SQRT(n)  (sqrt((n)))
-#define OS_ABS(n)   (fabs((n)))
-#define OS_MOD(n,m) (fmod((n),(m)))
-
-// Trig functions
-#define OS_SIN(n)     (sin((n)))
-#define OS_COS(n)     (cos((n)))
-#define OS_TAN(n)     (tan((n)))
-#define OS_ASIN(n)    (asin((n)))
-#define OS_ACOS(n)    (acos((n)))
-#define OS_ATAN(n)    (atan((n)))
-#define OS_ATAN2(y,x) (atan2((y),(x)))
+#ifndef USE_AVR_F64
+	#include <math.h>
+	
+	// Use double precision floats
+	#define OS_NUM_T double
+	
+	// Do whatever is needed to represent a literal. Assumes the argument is a
+	// C double.
+	#define OS_NUM_LIT(n) (n)
+	
+	// Convert to an integer (flooring the value)
+	#define OS_NUM2LONG(n) ((long)(n))
+	
+	// Convert an integer to a number
+	#define OS_LONG2NUM(i) ((double)(i))
+	
+	// Compare: 0 if equal, -ve if a < b, +ve if a > b
+	#define OS_CMP(a,b) ((a)-(b))
+	
+	// Basic arithmetic
+	#define OS_ADD(a,b) ((a)+(b))
+	#define OS_SUB(a,b) ((a)-(b))
+	#define OS_MUL(a,b) ((a)*(b))
+	#define OS_DIV(a,b) ((a)/(b))
+	
+	// Advanced arithmetic
+	#define OS_POW(n,p) (pow((n),(p)))
+	#define OS_SQRT(n)  (sqrt((n)))
+	#define OS_ABS(n)   (fabs((n)))
+	#define OS_MOD(n,m) (fmod((n),(m)))
+	
+	// Trig functions
+	#define OS_SIN(n)     (sin((n)))
+	#define OS_COS(n)     (cos((n)))
+	#define OS_TAN(n)     (tan((n)))
+	#define OS_ASIN(n)    (asin((n)))
+	#define OS_ACOS(n)    (acos((n)))
+	#define OS_ATAN(n)    (atan((n)))
+	#define OS_ATAN2(y,x) (atan2((y),(x)))
+#else /* USE_AVR_F64*/
+	#include <avr_f64.h>
+	
+	// Use double precision floats
+	#define OS_NUM_T float_64_t
+	
+	// Do whatever is needed to represent a literal. Assumes the argument is a
+	// C double. (To be converted by a pre-processing pass)
+	#define OS_NUM_LIT(n) (n)
+	
+	// Convert to an integer (flooring the value)
+	#define OS_NUM2LONG(n) (f_float64_to_long((n)))
+	
+	// Convert an integer to a number
+	#define OS_LONG2NUM(i) (f_long_to_float64((i)))
+	
+	// Compare: 0 if equal, -ve if a < b, +ve if a > b
+	#define OS_CMP(a,b) (f_compare((a), (b)))
+	
+	// Basic arithmetic
+	#define OS_ADD(a,b) (f_add((a),(b)))
+	#define OS_SUB(a,b) (f_sub((a),(b)))
+	#define OS_MUL(a,b) (f_mult((a),(b)))
+	#define OS_DIV(a,b) (f_div((a),(b)))
+	
+	// Advanced arithmetic
+	#define OS_POW(n,p) (f_pow((n),(p)))
+	#define OS_SQRT(n)  (f_sqrt((n)))
+	#define OS_ABS(n)   (f_abs((n)))
+	#define OS_MOD(n,m) (f_mod((n),(m)))
+	
+	// Trig functions
+	#define OS_SIN(n)     (f_sin((n)))
+	#define OS_COS(n)     (f_cos((n)))
+	#define OS_TAN(n)     (f_tan((n)))
+	#define OS_ASIN(n)    (f_arcsin((n)))
+	#define OS_ACOS(n)    (f_arccos((n)))
+	#define OS_ATAN(n)    (f_arctan((n)))
+	#define OS_ATAN2(y,x) (f_arctan2((y),(x)))
+#endif
 
 // Constants
 #define PI             (OS_NUM_LIT(3.141592653589793))
